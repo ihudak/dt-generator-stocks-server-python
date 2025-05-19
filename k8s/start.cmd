c6756122985ea6e4732ba1fb9f1c44cf5df168f5
Ivan Gudak <ivan.gudak@dynatrace.com>
2025-05-19 11:53:04 +0200

    Add start scripts

--- k8s/start.cmd
+++ k8s/start.cmd
@@ -2,9 +2,7 @@ kubectl apply -f namespace.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f secret.yaml
 kubectl apply -f database.yaml
-read -t 15 -p "Wait till the databases get up and running..."
-echo
+timeout 15
 kubectl apply -f stocks_server.yaml
-read -t 15 -p "Wait till the server get up and running..."
-echo
+timeout 15
 kubectl apply -f stocks_client.yaml
