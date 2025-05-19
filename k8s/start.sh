e0fd2ae4130983dd84bf8d504db0c44fa9b3b024
Ivan Gudak <ivan.gudak@dynatrace.com>
2025-05-19 11:39:16 +0200

    Add start script

---
+++ k8s/start.sh
@@ -0,0 +1,6 @@
+kubectl apply -f namespace.yaml
+kubectl apply -f configmap.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f database.yaml
+kubectl apply -f stocks_server.yaml
+kubectl apply -f stocks_client.yaml
